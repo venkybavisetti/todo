3debdb63860847f730e03ac816d1c283f5d19b30
venkatesh <58026249+venkybavisetti@users.noreply.github.com>
2020-09-05 10:41:31 +0530

    moved all hooks components to seperate app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,8 @@
-/* eslint-disable */
-
 import React from 'react';
-// import Todo from './components/Todo.js';
-// import Counter from './practiceHooks/stateHook/Counter';
-// import Toggle from './practiceHooks/stateHook/Toggle';
-// import Hoc from './practiceHooks/stateHook/Hoc';
-// import Timer from './practiceHooks/effectHook/Timer';
-import PokemonGallery from './practiceHooks/effectHook/Pokemon';
+import Todo from './components/Todo.js';
 
 const App = () => {
-  return (
-    // <Todo />
-    // <Counter />
-    // <Toggle />
-    // <Hoc />
-    // <Timer />
-    <PokemonGallery />
-  );
+  return <Todo />;
 };
 
 export default App;
